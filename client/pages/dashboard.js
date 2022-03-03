1ba9b35ea31b7b01dfa4f4b7a62ab5eb947df677
ian038 <ianphua3@gmail.com>
2022-03-03 13:18:10 -0500

    Completed app

--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -6,30 +6,30 @@ import { ThirdwebSDK } from "@3rdweb/sdk";
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 
-// const sdk = new ThirdwebSDK(
-//   new ethers.Wallet(process.env.NEXT_PUBLIC_METAMASK_KEY, ethers.getDefaultProvider("https://eth-rinkeby.alchemyapi.io/v2/aomq9bAqKPXysSkCOV8vhCC3yWO1snAH")
-//   )
-// );
+const sdk = new ThirdwebSDK(
+  new ethers.Wallet(process.env.NEXT_PUBLIC_METAMASK_KEY, ethers.getDefaultProvider("https://eth-rinkeby.alchemyapi.io/v2/aomq9bAqKPXysSkCOV8vhCC3yWO1snAH")
+  )
+);
 
 function Dashboard({ address }) {
   const [sanityTokens, setSanityTokens] = useState([]);
   const [thirdWebTokens, setThirdWebTokens] = useState([]);
 
-//   useEffect(() => {
-//     const getSanityAndThirdWebTokens = async () => {
-//       const coins = await fetch(
-//         "https://zhv96q28.api.sanity.io/v1/data/query/production?query=*%5B_type%3D%3D'Coins'%5D%7B%0A%20%20name%2C%0A%20%20usdPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%0A%7D"
-//       );
-//       const sanityToken = (await coins.json()).result;
-//       setSanityTokens(sanityToken);
-//       console.log(sanityToken);
+  useEffect(() => {
+    const getSanityAndThirdWebTokens = async () => {
+      const coins = await fetch(
+        "https://zhv96q28.api.sanity.io/v1/data/query/production?query=*%5B_type%3D%3D'Coins'%5D%7B%0A%20%20name%2C%0A%20%20usdPrice%2C%0A%20%20contractAddress%2C%0A%20%20symbol%2C%0A%20%20logo%0A%7D"
+      );
+      const sanityToken = (await coins.json()).result;
+      setSanityTokens(sanityToken);
+      console.log(sanityToken);
 
-//       setThirdWebTokens(
-//         sanityTokens.map((token) => sdk.getTokenModule(token.contractAddress))
-//       );
-//     };
-//     return getSanityAndThirdWebTokens();
-//   }, []);
+      setThirdWebTokens(
+        sanityTokens.map((token) => sdk.getTokenModule(token.contractAddress))
+      );
+    };
+    return getSanityAndThirdWebTokens();
+  }, []);
 
   return (
     <Wrapper>
